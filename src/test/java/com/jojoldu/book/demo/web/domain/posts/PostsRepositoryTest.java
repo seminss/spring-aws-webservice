011be18c8a5d7a3afc6c0840090e7e662f97c2c9
seminss <2007ksm@naver.com>
2023-02-21 17:43:37 +0900

    JPA auditing 기능 추가

--- src/test/java/com/jojoldu/book/demo/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jojoldu/book/demo/web/domain/posts/PostsRepositoryTest.java
@@ -14,6 +14,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -43,6 +45,30 @@ public class PostsRepositoryTest {
         Posts posts =postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+        //given
+        LocalDateTime now = LocalDateTime.of(2023,2,21,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList=postsRepository.findAll();
+
+        //then
+        Posts posts=postsList.get(0);
+
+        System.out.println(">>>>>>>>>>> createDate="+posts.getCreatedDate()+", modifiedDate= "+posts.getModifiedDate());
+
+
+        Assertions.assertThat(posts.getCreatedDate()).isAfter(now);
+        Assertions.assertThat(posts.getModifiedDate()).isAfter(now);
+
     }
 
     @AfterEach
