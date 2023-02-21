0face1bdc67272ea02175b9cd235072ff41d6320
seminss <2007ksm@naver.com>
2023-02-21 16:48:05 +0900

    post 기능구현, test 코드 추가

--- src/main/java/com/jojoldu/book/demo/service/posts/PostService.java
+++ src/main/java/com/jojoldu/book/demo/service/posts/PostService.java
@@ -1,7 +1,10 @@
 package com.jojoldu.book.demo.service.posts;
 
+import com.jojoldu.book.demo.domain.posts.Posts;
 import com.jojoldu.book.demo.domain.posts.PostsRepository;
 import com.jojoldu.book.demo.web.Dto.PostSaveRequestDto;
+import com.jojoldu.book.demo.web.PostResponseDto;
+import com.jojoldu.book.demo.web.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +17,18 @@ public class PostService {
     @Transactional
     public Long save(PostSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts=postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        posts.update(requestDto.getTitle(),requestDto.getContent()); //Post Entity(DB에 맞닿아있는)를 실제 update
+        return id;
+    }
+
+    public PostResponseDto findById(Long id) {
+        Posts entity=postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostResponseDto(entity); //response : 반환 객체
     }
 }
