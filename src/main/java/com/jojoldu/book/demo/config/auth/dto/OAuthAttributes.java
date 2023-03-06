4f2ea50cb8db3af71bee31430add5c64a771a263
seminss <2007ksm@naver.com>
2023-03-06 23:57:17 +0900

    네이버 로그인 구현

--- src/main/java/com/jojoldu/book/demo/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoldu/book/demo/config/auth/dto/OAuthAttributes.java
@@ -1,11 +1,11 @@
 package com.jojoldu.book.demo.config.auth.dto;
 
 import com.jojoldu.book.demo.domain.user.Role;
+import com.jojoldu.book.demo.domain.user.User;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.Map;
-import com.jojoldu.book.demo.domain.user.User;
 
 @Getter
 public class OAuthAttributes {
@@ -16,23 +16,22 @@ public class OAuthAttributes {
     private String picture;
 
     @Builder
-    public OAuthAttributes(Map<String, Object> attributes,
-                           String nameAttributeKey,String name,
-                           String email, String picture){
-        this.attributes=attributes;
-        this.nameAttributeKey=nameAttributeKey;
-        this.name=name;
-        this.email=email;
-        this.picture=picture;
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
     }
 
-    //of(): OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나 반환해야 한다.
-    public static OAuthAttributes of(String registrationId, String userNameAttributeName,
-                                     Map<String, Object> attributes){
-        return ofGoogle(userNameAttributeName,attributes);
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
+        return ofGoogle(userNameAttributeName, attributes);
     }
 
-    //User 엔티티 생성
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
@@ -43,7 +42,19 @@ public class OAuthAttributes {
                 .build();
     }
 
-    public User toEntity(){
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public User toEntity() {
         return User.builder()
                 .name(name)
                 .email(email)
